d1eb081652097ec4ffabe81fba3771fe586ddc83
guchimina <kazu9sum.mi@gmail.com>
2020-10-15 03:05:04 +0900

    ループ期間設定

--- src/viewer/User.jsx
+++ src/viewer/User.jsx
@@ -15,7 +15,8 @@ function User () {
     useEffect(() => {
         if (!cookies.user_id || cookies.user_id===undefined) fetchUserId()
         checkTicketAlready()
-        loopFetchState(10000)
+        const loopTime = 10 //ループ時間(自動更新)
+        loopFetchState(loopTime*1000)
     }, []);
 
     function sleep(time) {
@@ -50,7 +51,7 @@ function User () {
         try {
             const response = await axios.get(process.env.REACT_APP_API_URL + 'get_user_id.php')
             const data = response.data.id
-            const cookieExpiration = '2020-11-22 23:59:59'
+            const cookieExpiration = '2020-11-22 23:59:59' //cookie有効期限
             const cookieDate = new Date(cookieExpiration)
             
             setCookie('user_id', data, { expires: cookieDate, path: '/' })
